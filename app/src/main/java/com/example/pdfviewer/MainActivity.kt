07e1f4a3ae9b64fb7347fedec9d09090db0d5842
Arpit Arpit Jinal <arpit@arpits-mbp.home>
2024-01-06 16:06:24 -0500

    Initial commit

---
+++ app/src/main/java/com/example/pdfviewer/MainActivity.kt
@@ -0,0 +1,48 @@
+package com.example.pdfviewer
+
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.pdfviewer.ui.theme.PDFViewerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PDFViewerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    PdfViewer(uri = Uri.parse("https://www.africau.edu/images/default/sample.pdf"))
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PDFViewerTheme {
+        Greeting("Android")
+    }
+}
